684841521ac001a4c3c816e847c7aba463bdda31
F1-KS <f1-ks@mail.ru>
2023-02-19 14:01:11 +0300

    немного оптимизации кода

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -8,7 +8,13 @@ final class SingleImageViewController: UIViewController {
     
     
 //MARK: - «Научим» SingleViewController показывать разные картинки, не инициируя загрузку view
-    var image: UIImage!
+    
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return } // сначала мы проверяем, было ли ранее загружено view. Это очень важная проверка, необходимая, чтобы не закрэшиться, если view ещё не было загружено (и, соответственно, аутлет ещё не инициализирован). Именно эта проверка не даст нам закрэшиться из prepareForSegue
+            imageView.image = image // В эту точку мы не должны попадать из prepareForSegue. Мы можем попасть в неё, например, если был показан SingleImageViewController, а указатель на него был запомнен извне. Далее  — извне (например, по свайпу) в него проставляется новое изображение
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
