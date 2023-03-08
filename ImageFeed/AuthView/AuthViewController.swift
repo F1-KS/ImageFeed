f78b999a977fde32f04dc1f7ffb20b588d297fbe
F1-KS <f1-ks@mail.ru>
2023-03-08 22:42:23 +0300

    Реализована работа с WebView unsplash

--- ImageFeed/AuthView/AuthViewController.swift
+++ ImageFeed/AuthView/AuthViewController.swift
@@ -3,11 +3,27 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let ShowWebViewSegueIdentifier = "ShowWebView" //Добавим значение этого (ShowWebView) идентификатора в код класса AuthViewController — оно пригодится нам в дальнейшем.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
     
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
     
-    
-    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
 }
--- ImageFeed/Services/Constants.swift
+++ ImageFeed/Services/Constants.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-private let AccessKey = "65tyYxZbAmH16moEzFZdIMKz1bftUNe9Wegzqn2TY24"
-private let SecretKey = "2kh9yN0VN1c5R_rhTOmLFw88yoTVj1RgneIiYq8mQTw"
-private let RedirectURI = "https://unsplash.com/@f1_ks"
-private let AccessScope = "public+read_user+write_likes"
-private var DefaultBaseURL: URL {
-    guard let url = URL(string: "https://unsplash.com") else {
+let AccessKey = "65tyYxZbAmH16moEzFZdIMKz1bftUNe9Wegzqn2TY24"
+let SecretKey = "2kh9yN0VN1c5R_rhTOmLFw88yoTVj1RgneIiYq8mQTw"
+let RedirectURI = "https://unsplash.com/@f1_ks"
+let AccessScope = "public+read_user+write_likes"
+var DefaultBaseURL: URL {
+    guard let url = URL(string: "https://api.unsplash.com") else {
         preconditionFailure("Не удалось получить доступ к unsplash.com")
     }
     return url
